f295416f980ad6ab6c462c73bbc6742f45fd4d1b
pallavi31-commits <govindraopallavi@gmail.com>
2026-01-18 22:50:36 +0530

    day 3

---
+++ module 1/8forLoop.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a number : ";
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        if(i%2 == 0){
+            cout<<i<<" is even number"<<endl;
+        }
+        else{
+            cout<<i<<" is odd number"<<endl;
+        }
+    }
+
+    //nested for loop
+
+}
