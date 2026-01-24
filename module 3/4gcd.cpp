cff556d8b3227f66fa550ee7aa983817bcbfa8bf
pallavi31-commits <govindraopallavi@gmail.com>
2026-01-24 23:15:45 +0530

    day 9

---
+++ module 3/4gcd.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    int GCD(int n1,int n2){
+        int ans = 1;
+        for(int i=1;i<=min(n1,n2);i++){
+            if(n1%i == 0 && n2%i == 0){
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    Solution sol;
+    int p,q;
+    cout<<"Enter two number : ";
+    cin>>p>>q;
+    cout<<sol.GCD(p,q);
+
+}
