9cbb5033a0678bbc906bfd09ad338a47ec2912cc
pallavi31-commits <govindraopallavi@gmail.com>
2026-01-26 18:12:33 +0530

    day 11

---
+++ 2patternPrinting/13naturalNoTriangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter number of row : ";
+    cin>>n;
+
+    int x = 1;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<x<<" ";
+            x++;
+        }
+        cout<<endl;
+    }
+}
